02a5a0fd1531e5649864d58185f873ec1fc74ecd
Delpo <guido_delponte@hotmail.com>
2025-07-29 20:35:32 -0300

    test CSRF

--- ssn/config/settings/prod.py
+++ ssn/config/settings/prod.py
@@ -3,9 +3,14 @@ from decouple import Csv, config
 from .base import *
 
 # --- CONFIGURACIÓN DE PRODUCCIÓN ---
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True #config("DEBUG", default=False, cast=bool)
 SECRET_KEY = config("SECRET_KEY")
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{config('SSL_DOMAIN')}",
+    f"https://{config('SSL_IP')}"
+]
 
 # Cargar configuración de Logging para producción
 LOGGING = get_logging_config(debug_mode=DEBUG, logs_dir=LOGS_DIR, apps=LOGGING_APPS)
